a2dc3dbc62bdc81d3412877c993c8e40ee2001fa
Salar Mirza <sallarmirza77@gmail.com>
2025-08-20 09:46:09 +0500

    making changes

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Navbar, Nav } from "react-bootstrap"; 
+import { Navbar, Nav, Container } from "react-bootstrap";
 import Home from "./Home";
 import Services from "./Services";
 import { Gallery } from "./Gallery";
@@ -7,48 +7,62 @@ import { Booking } from "./Booking";
 import { Stylist } from "./Stylist";
 import { Profile } from "./Profile";
 
-function NavigationBar() {   
-    const [tabs, setTabs] = useState('Home'); 
+function NavigationBar() {
+  const [tabs, setTabs] = useState("Home");
 
-    function showTabs() {
-        switch (tabs) {
-            case 'Home':
-                return <Home />;
-            case 'Gallery':
-                return <Gallery />;
-            case 'Services':
-                return <Services />;
-            case 'Booking': 
-                return <Booking />;
-            case 'Stylist':
-                return <Stylist/>;
-            case 'Profiles':
-                return <Profile/>
-            default:
-                return <Home />; 
-        }
+  function showTabs() {
+    switch (tabs) {
+      case "Home":
+        return <Home />;
+      case "Gallery":
+        return <Gallery />;
+      case "Services":
+        return <Services />;
+      case "Booking":
+        return <Booking />;
+      case "Stylist":
+        return <Stylist />;
+      case "Profiles":
+        return <Profile />;
+      default:
+        return <Home />;
     }
+  }
 
-    return (
-        <div>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">
-                    <span style={{ fontFamily: 'Great Vibes', fontSize: '1.8rem' }}>Saloon</span>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav"> 
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" onClick={() => setTabs('Home')}>Home</Nav.Link>
-                        <Nav.Link href="#gallery" onClick={() => setTabs('Gallery')}>Gallery</Nav.Link> 
-                        <Nav.Link href="#services" onClick={() => setTabs('Services')}>Services</Nav.Link>
-                        <Nav.Link href="#booking" onClick={() => setTabs('Booking')}>Booking</Nav.Link> 
-                        <Nav.Link href="#Stylist" onClick={() => setTabs('Stylist')}>Stylist </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            {showTabs()}
-        </div>
-    );
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          <span style={{ fontFamily: "Great Vibes", fontSize: "1.8rem" }}>
+            Saloon
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" onClick={() => setTabs("Home")}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#gallery" onClick={() => setTabs("Gallery")}>
+              Gallery
+            </Nav.Link>
+            <Nav.Link href="#services" onClick={() => setTabs("Services")}>
+              Services
+            </Nav.Link>
+            <Nav.Link href="#booking" onClick={() => setTabs("Booking")}>
+              Booking
+            </Nav.Link>
+            <Nav.Link href="#Stylist" onClick={() => setTabs("Stylist")}>
+              Stylist{" "}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container fluid className="py-3">
+        {showTabs()}
+      </Container>
+    </div>
+  );
 }
 
 export default NavigationBar;
